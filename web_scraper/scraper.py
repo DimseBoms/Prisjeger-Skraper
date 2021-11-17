c97b30078bb025775ac69edc9febbd9b0261f8b0
DimseBoms <72513291+DimseBoms@users.noreply.github.com>
2021-11-17 14:26:11 +0100

    Revidert skriptet og gjort det mer stabilt
    1. Gått gjennom skriptet i sin helhet og fjernet unødvendige deler.
    2. Skriptet henter nå elementer basert på attributt navn isteden for å gjennom DOM-treet med en fullstendig URL. Dermed reduseres risikoen for at små endringen på nettsiden bryter skriptets funksjon.
    3. Skriptet henter nå butikkdata i tillegg til prisdata og legger denne inn sammen med prisene i egen .json.
    4. All lagret .json data blir printet penere for lettere lesbarhet.

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,65 +1,50 @@
-# Laget av Dmitriy Safiullin. Sist oppdatert: 10.10.2021
+# Laget av Dmitriy Safiullin. Sist oppdatert: 17.11.2021
 # Programmet henter prisdata fra enhver.no. Prisdata blir lagret i ./data mappen.
 
-# Importsetninger
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from datetime import datetime
 from datetime import date
+import os
 import json
 
-# Globale variabler
-NETTSIDE_URL = 'https://enhver.no/priser/'
-DRIVER_URL = "/usr/lib/chromium-browser/chromedriver"
-TR_XPATH = "/html/body/div/div/div/div/div/div/div/table/tbody/tr[@class='product']"
-
 start_tid = datetime.now()
-print(f"Start: {start_tid}")
+print(f'Startet: {start_tid}.')
 
 # Oppsett av virtuell nettleser
+NETTSIDE_URL = 'https://enhver.no/priser/'
+DRIVER_URL = "/usr/lib/chromium-browser/chromedriver"
 options = Options()
 options.headless = True
-options.add_argument("--window-size=1920,1080")
+options.add_argument('--window-size=1920,1080')
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_URL)
 driver.get(NETTSIDE_URL)
 
-# TODO:
-# Finn ut hvilke butikker som er med i tabellen og hvilke indexer de
-# hører til. Butikkdataen skal deretter settes inn som et
-# eget nøkkel -> verdier forhold i produkter{} i samme stil som
-# produktene selv.
-# Eksempel: Butikker: ['Kiwi', 'Meny', 'osv', 'osv']
-
-# Hent produktnavn og sett de inn som nøkler i produkter{}
-
-# Teller produkter som er funnet
-antall_produkter = len(driver.find_elements_by_xpath(TR_XPATH))
-antall_butikker = len(driver.find_element_by_xpath(TR_XPATH).find_elements_by_tag_name("td")) - 1
-print("Antall produkter funnet: " + str(antall_produkter))
-print("Antall butikker funnet: " + str(antall_butikker))
-
-# Henter prisinformasjon og putter den inn som verdier i produkter{}
-produkter = {}
-print("Produkter:")
-for rad in driver.find_elements_by_xpath(TR_XPATH):
-    produkt = rad.find_element_by_tag_name("td").text.replace("\n", ", ")
+# Henter data og setter den inn i produkter{}
+produkter = {'butikker': [], 'varer': {}}
+# Henter butikkdata
+butikker = []
+for butikk_span in driver.find_elements_by_xpath("//span[@class='retailer-name']"):
+    butikk = butikk_span.find_element_by_tag_name('img').get_attribute('title')
+    butikker.append(butikk)
+produkter.update({'butikker': butikker})
+# Henter prisdata
+for rad in driver.find_elements_by_xpath("//tr[@class='product']"):
+    produkt = rad.find_element_by_tag_name('td').text.replace('\n', ', ')
     pris_liste_tmp = []
     col_index = 0
-    for celle in rad.find_elements_by_tag_name("td"):
-        if col_index != 0 :
+    for celle in rad.find_elements_by_tag_name('td'):
+        if col_index != 0:
             pris_liste_tmp.append(celle.text)
         col_index += 1
-    print(str(produkt) + ": " + str(pris_liste_tmp))
-    produkter.update({produkt: pris_liste_tmp})
+    produkter['varer'].update({produkt: pris_liste_tmp})
 driver.quit()
 
-# TODO:
-# Feilhåndtering for I/O. Dersom mappen ikke eksisterer skal
-# skal den lages mens programmet kjøres.
-
-# Skriver data til fil
-fil = f"./data/produkter-{date.today()}.json"
+# Lager mappen data dersom den ikke eksisterer og skriver data til fil
+if not os.path.isdir('./data'):
+    os.mkdir('./data')
+fil = f'./data/produkter-{date.today()}.json'
 with open(fil, "w") as outfile:
-    json.dump(produkter, outfile)
-    print(f"Suksess. Data skrevet til {fil}")
-print(f"Kjøretid: {datetime.now() - start_tid}")
+    json.dump(produkter, outfile, sort_keys=True, indent=2)
+    print(f'Suksess. Data skrevet til {fil}.')
+print(f'Kjøretid: {datetime.now() - start_tid}.')
